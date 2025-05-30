070bd0d2bb7d033ac9ab5f0640eefaab1a1c7f5e
DSeaNguyen <nguyendinhhoangquan1825@gmail.com>
2025-05-30 22:02:34 +0700

    Modify Login, Add Register

--- FE/app/src/main/java/com/example/tqt_quiz/presentation/view/activity/Register.java
+++ FE/app/src/main/java/com/example/tqt_quiz/presentation/view/activity/Register.java
@@ -1,8 +1,6 @@
 package com.example.tqt_quiz.presentation.view.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,34 +9,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.tqt_quiz.R;
-import com.example.tqt_quiz.presentation.contract_vp.MainActitvityContract;
-import com.example.tqt_quiz.presentation.presenter.MainActivityPresenter;
 
-public class MainActivity extends AppCompatActivity implements MainActitvityContract.IView {
-    private MainActitvityContract.IPresenter mainActivityPresenter = null;
+public class Register extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        mainActivityPresenter = new MainActivityPresenter(this);
-        mainActivityPresenter.onCreateActivity();
-
-
-        Intent intent = new Intent(this, Register.class);
-        startActivity(intent);
-
-        finish();
-    }
-
-    @Override
-    public void showToast(String data) {
-        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
     }
 }
