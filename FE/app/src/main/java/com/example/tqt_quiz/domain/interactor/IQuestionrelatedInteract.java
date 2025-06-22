6d436027d331b088eef5dca9bb0b025287235d1f
duongthuan0405 <duongthuan0405@gmail.com>
2025-06-22 15:10:56 +0700

    Modify Create Quiz

--- FE/app/src/main/java/com/example/tqt_quiz/domain/interactor/IQuestionrelatedInteract.java
+++ FE/app/src/main/java/com/example/tqt_quiz/domain/interactor/IQuestionrelatedInteract.java
@@ -4,12 +4,11 @@ import android.content.Context;
 
 import com.example.tqt_quiz.domain.dto.CreateQuestionRequest;
 import com.example.tqt_quiz.domain.dto.QuestionDTO;
-import com.example.tqt_quiz.domain.dto.QuizDTO;
 
 import java.util.List;
 
 public interface IQuestionrelatedInteract {
-    void CreateQuestion (CreateQuestionRequest questionRequest, Context context,CreateQuestionCallBack callback);
+    void CreateQuestion (List<CreateQuestionRequest> questionRequest, Context context, CreateQuestionCallBack callback);
     public interface CreateQuestionCallBack
     {
         void onSuccess();
